ddfd00104af81871537dd1a98b6f2c34b16f7843
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2021-11-22 09:05:02 +0200

    added date decoding strategy

--- ProjectNASA/Networking/APICaller.swift
+++ ProjectNASA/Networking/APICaller.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let NASAUrl =  URL(string: "https://images-api.nasa.gov/search?q=%22%22")
+    }
+    
+    private init() {}
+    
+    public func getNASAImageData(completion: @escaping (Result<[Item], Error>) -> Void) {
+        guard let url = Constants.NASAUrl else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            else if let data = data {
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    let result = try decoder.decode(APIResponse.self, from: data)
+                    
+                    DispatchQueue.main.async {
+                        print("Images Count: \(result.collection.items?.count)")
+                        completion(.success(result.collection.items!))
+                    }
+                    
+                    
+                }
+                catch {
+                    completion(.failure(error))
+            }
+        }
+    }
+        
+    task.resume()
+        
+   }
+}
+    
+   
+
+
